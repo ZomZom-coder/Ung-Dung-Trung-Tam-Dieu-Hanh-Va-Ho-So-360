9e1b00aa69995c92b19b8e5492df1098cb88255f
nicehelp <thientest911@gmail.com>
2026-05-03 17:01:44 +0700

    Add files via upload

---
+++ Trung tam dieu hanh va ho so 360/Trung tam dieu hanh va ho so 360/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trung_tam_dieu_hanh_va_ho_so_360.Stores;
+
+namespace Trung_tam_dieu_hanh_va_ho_so_360.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        // Bind vào Content Control trong XAML
+        public ViewModelBase CurrenViewModel => _navigationStore.CurrentViewModel;
+            
+       
+        public MainViewModel(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+
+            _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrenViewModel));
+        }
+
+    }
+}
